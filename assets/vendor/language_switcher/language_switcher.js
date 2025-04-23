8807e1444afee7b75e63635bb1d096adbc1cb3ec
EsChris <themainchris@gmail.com>
2025-04-23 13:13:44 +0100

    data analyst portfolio

---
+++ assets/vendor/language_switcher/language_switcher.js
@@ -0,0 +1,7 @@
+
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
+}
+
+{/* <script src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script> */}
+
